df7fdd975e988377094ea24374bd6191c3a21bcb
kb0rg <kb0rg@users.noreply.github.com>
2017-09-16 14:58:31 -0700

    Add line color var

--- src/drawing/lines.clj
+++ src/drawing/lines.clj
@@ -8,14 +8,19 @@
 
 (defn draw []
   ; call quil bg func
-  (q/background 240)
+  
+  (q/background 240) ; remove bg to make lines cumulative
+  (q/stroke 0 255 0)
   (q/line 0 0 (q/mouse-x) (q/mouse-y))
-  (q/line 200 0 (q/mouse-x) (q/mouse-y))
-  (q/line 0 200 (q/mouse-x) (q/mouse-y))
-  (q/line 200 200 (q/mouse-x) (q/mouse-y)))
+  (q/stroke 0 0 255)
+  (q/line 500 0 (q/mouse-x) (q/mouse-y))
+  (q/stroke 0 255 255)
+  (q/line 0 500 (q/mouse-x) (q/mouse-y))
+  (q/stroke 0 255 125)
+  (q/line 500 500 (q/mouse-x) (q/mouse-y)))
 
 (q/defsketch hello-lines
-  :title "You can see lines"
+  :title "kborg does or does not do lines"
   :size [500 500]
   :setup setup
   :draw draw
