4d17acb073921139cb40877947876662c0839e7f
kb0rg <kb0rg@users.noreply.github.com>
2017-09-16 14:35:06 -0700

    Add ns and setup

--- src/drawing/lines.clj
+++ src/drawing/lines.clj
@@ -1,1 +1,11 @@
+(ns drawing.lines
+  (:require [quil.core :as q]))
 
+(defn setup []
+  (q/frame-rate 30)
+  (q/color-mode :rgb)
+  (q/stroke 255 0 0))
+
+(defn draw []
+  ; call quil bg func
+  (q/background 240))
